c9dd95e3d0f8c00f8597156c07a230dd0b3bcd02
Gracielo Justine <cielo.justine01@gmail.com>
2021-05-15 22:02:59 +0700

    add viewbinding, edit kotlin&gradle version

--- app/src/main/java/com/lukitor/myapplicationC/MainActivity.kt
+++ app/src/main/java/com/lukitor/myapplicationC/MainActivity.kt
@@ -2,10 +2,13 @@ package com.lukitor.myapplicationC
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lukitor.myapplicationC.databinding.ActivityMainBinding
 
+lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
